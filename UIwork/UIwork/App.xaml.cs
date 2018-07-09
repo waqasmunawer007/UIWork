1fce6314a9fa75265277e47f791a365755c7cd5b
waqasmunawer007 <waqasmunawer007@gmail.com>
2018-07-09 21:24:25 +0500

    profile page added

--- UIwork/UIwork/App.xaml.cs
+++ UIwork/UIwork/App.xaml.cs
@@ -11,7 +11,7 @@ namespace UIwork
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+			MainPage = new MainPage();
         }
 
         protected override void OnStart()
